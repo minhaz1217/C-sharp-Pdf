e3e4b1af4a54a78a98f9acfb24edbba50174cf25
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-11-14 00:07:19 +0600

    added dependency

--- C-sharp-Pdf/Form1.cs
+++ C-sharp-Pdf/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using PdfSharp;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
 
 namespace C_sharp_Pdf
 {
@@ -15,6 +19,43 @@ namespace C_sharp_Pdf
         public Form1()
         {
             InitializeComponent();
+        }
+        public string urlCodeForces = "http://codeforces.com/contest/924/problem/C";
+        public string getCodeForces(string url)
+        {
+            var web = new HtmlWeb();
+            var html = web.Load(url);
+            var ht = html.DocumentNode.SelectNodes("//div");
+            foreach (var i in ht)
+            {
+                if (i.HasClass("problem-statement"))
+                {
+                    return i.InnerHtml;
+                }
+            }
+            return "";
+        }
+
+        public void generatePDF(string pdfString)
+        {
+
+
+            PdfDocument pdf = PdfGenerator.GeneratePdf(pdfString, PageSize.A4);
+            //PdfDocument pdf = PdfGenerator.GeneratePdf("<p><h1>Hello World</h1>This is html rendered text</p>", PageSize.A4);
+            pdf.Save("document.pdf");
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string myString = getCodeForces(urlCodeForces);
+            richTextBox1.Text = myString;
+            if (myString != "")
+            {
+                generatePDF(myString);
+            }
         }
     }
 }
